7ffea51d5b8217b1600de483bd6e815a4e33dedb
Ahmedsamehm <a7medxd992@hotmail.com>
2025-06-17 20:57:06 +0300

    Updates

---
+++ assets/Loading-EoMBAaDS.js
@@ -0,0 +1,6 @@
+import{c as a,j as e}from"./index-DB_8zrqn.js";/**
+ * @license lucide-react v0.507.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],s=a("loader-circle",c);function n(){return e.jsx("div",{className:"flex items-center justify-center min-h-[calc(100vh-120px)] ",children:e.jsx(s,{className:"animate-spin",size:70})})}export{n as L,s as a};
